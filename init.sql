c452d7b3105664e718c0ac0a90bbcac1be030bca
emarkula24 <emarkula24@students.oulu.fi>
2025-07-02 15:15:55 +0300

    improved docker compose and added sql init scripts

---
+++ init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(80) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS refreshtokens (
+    user_id INT UNSIGNED NOT NULL,
+    jti CHAR(36) NOT NULL UNIQUE,
+    refresh_token VARCHAR(512) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+)
