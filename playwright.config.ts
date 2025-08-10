213ffbddc90542ae7420d72eb57e0aaf4c44f729
emarkula24 <emarkula24@student.oulu.fi>
2025-08-10 11:51:45 +0300

    modified playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  timeout: 3000,
+  timeout: 120 * 1000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -27,7 +27,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
+    storageState: 'state.json',
+    actionTimeout: 5000,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,9 +74,26 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: [
+    {
+    command: 'go run .',
+    url: 'http://localhost:8081',
+    name: 'Backend',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+    gracefulShutdown: {signal: 'SIGTERM', timeout: 1000},
+    cwd: '/api'
+  },
+  {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    name: 'Frontend',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  }, 
+  // { command: 'docker compose up -f -d . ',
+  //   cwd: '/api'
+  // }
+],
+
 });
