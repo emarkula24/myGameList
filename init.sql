4ce649ac250dfc7de25ee662c2e7d55c15a44bac
emarkula24 <emarkula24@student.oulu.fi>
2025-08-21 16:56:41 +0300

    updated production init sql

--- init.sql
+++ init.sql
@@ -9,9 +9,24 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS refreshtokens (
-    user_id INT UNSIGNED NOT NULL,
-    jti CHAR(36) NOT NULL UNIQUE,
+    user_id INT UNSIGNED NOT NULL UNIQUE,
+    jti CHAR(36) NOT NULL,
     refresh_token VARCHAR(512) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS games (
+    game_id INT UNSIGNED NOT NULL,
+    gamename VARCHAR(255),
+    PRIMARY KEY (game_id)
+);
+
+CREATE TABLE IF NOT EXISTS user_games (
+    username VARCHAR(80) NOT NULL,
+    game_id INT UNSIGNED NOT NULL,
+    status INT NOT NULL,
+    PRIMARY KEY (username, game_id),
+    FOREIGN KEY (username) REFERENCES users(username),
+    FOREIGN KEY (game_id) REFERENCES games(game_id)
 );
